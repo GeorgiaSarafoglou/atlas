8fdc16ba62350eeb4e0d7ad37cb4a7693455340f
rigas2k19 <rigas2k19@yandex.com>
2023-01-22 20:51:03 +0200

    added breadcrums

--- php/breadcrumbs.php
+++ php/breadcrumbs.php
@@ -23,6 +23,20 @@
             $breadcrumb = 'Αρχική Σελίδα > Θέσεις Πρακτικής';
             break;
         case 'job-details.php':
+            if($previous_page == 'job-details.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Θέσεις Πρακτικής > Λεπτομέρειες Θέσης > Αγαπημένα';
+            }
+            else if($previous_page == 'watch-jobs.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Όλες οι Αγγελίες > Λεπτομέρειες Θέσης';
+            }
+            else if($previous_page == 'watch-jobs-temp.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Προσωρινές Αγγελίες > Λεπτομέρειες Θέσης';
+            }
+            else{
+                $breadcrumb = 'Αρχική Σελίδα > Αγαπημένα';
+            }
+            break;
+
             $breadcrumb = 'Αρχική Σελίδα > Θέσεις Πρακτικής > Λεπτομέρειες Θέσης';
             break;
         case 'my-applications.php':
@@ -38,6 +52,9 @@
             break;
         case 'saved-applications.php':
             if($previous_page == 'job-details.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Θέσεις Πρακτικής > Λεπτομέρειες Θέσης > Αποθηκευμένες Αιτήσεις';
+            }
+            else if($previous_page == 'update-application.php'){
                 $breadcrumb = 'Αρχική Σελίδα > Θέσεις Πρακτικής > Λεπτομέρειες Θέσης > Αποθηκευμένες Αιτήσεις';
             }
             else{
@@ -58,6 +75,96 @@
         case 'rejected-applications.php':
             $breadcrumb = 'Αρχική Σελίδα > Απορριφθείσες Αιτήσεις';
             break;
+        case 'update-application.php':
+            if($previous_page == 'saved-applications'){
+                $breadcrumb = 'Αρχική Σελίδα > Αποθηκευμένες Αιτήσεις > Επεξεργασία Αίτησης';
+            }
+            else if($previous_page == 'my-applications.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Όλες οι Αιτήσεις > Επεξεργασία Αίτησης';
+            }
+            else{
+                $breadcrumb = 'Αρχική Σελίδα > Επεξεργασία Αίτησης';
+            }
+            break;
+        case 'create-job-offer.php':
+            $breadcrumb = 'Αρχική Σελίδα > Δημιουργία Αγγελίας';
+            break;
+        case 'watch-jobs-temp.php':
+            if($previous_page == 'create-job-offer.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Δημιουργία Αγγελίας > Προσωρινά Αποθηκευμένες Αγγελίες';
+            }
+            else{
+                $breadcrumb = 'Αρχική Σελίδα > Προσωρινά Αποθηκευμένες Αγγελίες';
+            }
+            break;
+        case 'watch-jobs-active.php':
+            if($previous_page == 'create-job-offer.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Δημιουργία Αγγελίας > Προσωρινά Αποθηκευμένες Αγγελίες';
+            }
+            else{
+                $breadcrumb = 'Αρχική Σελίδα > Ενεργές Αγγελίες';
+            }
+            break;
+        case 'edit-job.php':
+            if($previous_page == 'watch-jobs-temp.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Προσωρινά Αποθηκευμένες Αγγελίες > Επεξεργασία Αγγελίας';
+            }
+            else{
+                $breadcrumb = 'Αρχική Σελίδα > Προσωρινά Αποθηκευμένες Αγγελίες';
+            }
+            break;
+        case 'watch-jobs.php':
+            if($previous_page == 'watch-jobs.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Όλες οι Αγγελίες > ';
+            }
+            else{
+                $breadcrumb = 'Αρχική Σελίδα > Όλες οι Αγγελίες';
+            }
+            break;
+
+        case 'jobs-pending.php':
+            $breadcrumb = 'Αρχική Σελίδα > Εκκρεμείς Αιτήσεις ';
+            break;
+        case 'jobs-rejected.php':
+            $breadcrumb = 'Αρχική Σελίδα > Απορριφθείσες Αιτήσεις ';
+            break;
+        case 'jobs-approved.php':
+            $breadcrumb = 'Αρχική Σελίδα > Εγκεκριμένες Αιτήσεις ';
+            break;
+        case 'jobs-all.php':
+            $breadcrumb = 'Αρχική Σελίδα > Όλες οι Αιτήσεις ';
+            break;
+        case 'job-interest-details.php':
+            if($previous_page == 'jobs-all.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Όλες οι Αιτήσεις > Λεπτομέρειες Αίτησης';
+            }
+            else if($previous_page == 'jobs-pending.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Εκκρεμείς Αιτήσεις > Λεπτομέρειες Αίτησης';
+            }
+            else if($previous_page == 'jobs-approved.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Εγκεκριμένες Αιτήσεις > Λεπτομέρειες Αίτησης';
+            }
+            else if($previous_page == 'job-interest.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Εκκρεμείς Αιτήσεις > Απόρριψη Αίτησης > Λεπτομέρειες Αίτησης';
+            }
+            break;
+        case 'job-rejection-func.php':
+            if($previous_page == 'jobs-all.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Όλες οι Αιτήσεις > Λεπτομέρειες Αίτησης > Απόρριψη Αίτησης';
+            }
+            else if($previous_page == 'jobs-pending.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Εκκρεμείς Αιτήσεις > Απόρριψη Αίτησης';
+            }
+            else{
+                $breadcrumb = 'Αρχική Σελίδα > Εκκρεμείς Αιτήσεις > Απόρριψη Αίτησης';
+            }
+            break;
+        case 'job-interest.php':
+            if($previous_page == 'watch-jobs-active.php'){
+                $breadcrumb = 'Αρχική Σελίδα > Εκκρεμείς Αιτήσεις > Απόρριψη Αίτησης';
+            }
+            break;
+        
         default:
             $breadcrumb = '';
     }
@@ -65,3 +172,4 @@
     //echo $previous_page. '>' .$current_page;
     echo $breadcrumb;
 ?>
+<br>
